6165bed8acf0829f6f98614df57364b6f0bdc852
Dwayne Charrington <dwaynecharrington@gmail.com>
2021-01-06 22:34:16 +1000

    feat(app): get qr generation working

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,3 +1,15 @@
 export class MyApp {
-  public message = 'Hello World!';
+
+  private url: string = '';
+  private image = '';
+
+  private formSubmit() {
+    this.generate();
+  }
+
+  private async generate() {
+    if (this.url.trim() !== '') {
+      this.image = `https://www.qrtag.net/api/qr_4.png?url=${this.url}`;
+    }
+  }
 }
